fb04f79de819940ebbeb6668dd84d78ea7fa2fb6
JaylanChen <JaylanChen@126.com>
2020-03-17 11:19:54 +0800

    LPR 利率

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,30 +21,22 @@ Page({
     gjjFocus: false,
     businessFocus: false,
     startDateStr: '',
+    LPR: true,
+    LPRValue: 4.8,
+    basePoint: 0,
+    LPRValueText: '4.8',
+    basePointText: '0',
+    LPRRateValue: 4.8,
 
     paymentMethodIndex: 0,
     paymentYearIndex: 29,
+    businessLoanRateTypeIndex: 0,
     businessLoanRateIndex: 6,
     gjjLoanRateIndex: 0,
-    loanTypeArr: [{
-      text: '商业贷款',
-      id: 1,
-      checked: true
-    }, {
-      text: '公积金贷款',
-      id: 2
-    }, {
-      text: '组合贷款',
-      id: 3
-    }],
-    paymentMethodArr: [{
-      text: '等额本息(每月等额还款)',
-      id: 1
-    }, {
-      text: '等额本金(每月递减还款)',
-      id: 2
-    }],
+    loanTypeArr: [{ text: '商业贷款', id: 1, checked: true }, { text: '公积金贷款', id: 2 }, { text: '组合贷款', id: 3 }],
+    paymentMethodArr: [{ text: '等额本息(每月等额还款)', id: 1 }, { text: '等额本金(每月递减还款)', id: 2 }],
     paymentYearArr: [],
+    businessLoanRateTypeArr: [{ text: 'LPR', value: 1 }, { text: '基准利率', value: 2 }],
     businessLoanRateArr: [],
     gjjLoanRateArr: []
   },
@@ -52,15 +44,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    var currentDate = new Date();
-    var paymentYearArr = [];
+    let currentDate = new Date();
+    let paymentYearArr = [];
     for (let i = 1; i <= 30; i++) {
       paymentYearArr.push({
         value: i,
         text: `${i}年 (${i * 12}月)`
       });
     }
-    var tempArr = util.getBusinessLoanRateArr(this.data.paymentYear);
+    let tempArr = util.getBusinessLoanRateArr(this.data.paymentYear);
     this.setData({
       paymentYearArr: paymentYearArr,
       businessLoanRateArr: tempArr,
@@ -73,7 +65,7 @@ Page({
   // 公共方法
   resetData: function (loanType) {
     let paymentYear = 30;
-    var currentDate = new Date();
+    let currentDate = new Date();
     this.setData({
       loanType: loanType,
       paymentMethodIndex: 0,
@@ -91,16 +83,23 @@ Page({
 
     })
   },
+  /**
+   * 贷款计算数据
+   */
   getMortgageData: function () {
-    var data = {
+    let data = {
       businessTotalLoan: parseFloat(this.data.businessTotalLoan),
-      businessLoanRate: this.data.businessLoanRateArr[this.data.businessLoanRateIndex].value,
       gjjTotalLoan: parseFloat(this.data.gjjTotalLoan),
       gjjLoanRate: this.data.gjjLoanRateArr[this.data.gjjLoanRateIndex].value,
       paymentMethod: this.data.paymentMethodArr[this.data.paymentMethodIndex].id,
       paymentYear: this.data.paymentYearArr[this.data.paymentYearIndex].value,
       startDate: this.data.startDate,
       loanType: this.data.loanType
+    }
+    if (this.data.LPR) {
+      data.businessLoanRate = this.data.LPRRateValue;
+    } else {
+      data.businessLoanRate = this.data.businessLoanRateArr[this.data.businessLoanRateIndex].value;
     }
     return data;
   },
@@ -117,6 +116,9 @@ Page({
     })
     return;
   },
+  /**
+   * 商业贷款金额输入
+   */
   businessTotalLoanInput: function (e) {
     let value = e.detail.value || 0;
     if (value === 0) {
@@ -135,6 +137,9 @@ Page({
       })
     }
   },
+  /**
+   * 公积金贷款金额输入
+   */
   gjjTotalLoanInput: function (e) {
     let value = e.detail.value || 0.0;
     if (value === 0) {
@@ -153,45 +158,118 @@ Page({
       })
     }
   },
+  /**
+   * 贷款类型切换
+   */
   loanTypeChange: function (e) {
-    var loanType = e.detail.value;
+    let loanType = e.detail.value;
     this.resetData(loanType);
   },
+  /**
+   * 还款方式切换
+   */
   paymentMethodchange: function (e) {
-    var index = e.detail.value;
+    let index = e.detail.value;
     this.setData({
       paymentMethodIndex: index
     })
   },
+  /**
+   * 还款年数
+   */
   paymentYearchange: function (e) {
-    var index = e.detail.value;
-    var paymentYear = this.data.paymentYearArr[index].value;
+    let index = e.detail.value;
+    let paymentYear = this.data.paymentYearArr[index].value;
     this.setData({
       paymentYearIndex: index,
       businessLoanRateArr: util.getBusinessLoanRateArr(paymentYear),
       gjjLoanRateArr: util.getBusinessLoanRateArr(paymentYear)
     })
   },
+  /**
+   * 开始还款日期
+   */
   startDateChange: function (e) {
     this.setData({
       startDate: new Date(e.detail.value),
       startDateStr: util.formatDate(new Date(e.detail.value))
     });
   },
+  /**
+   * 利率类型变更
+   */
+  businessLoanRateTypeChange: function (e) {
+    let index = e.detail.value;
+    this.setData({
+      businessLoanRateTypeIndex: index,
+      LPR: index == 0
+    })
+  },
+  /**
+   * LPR提示
+   */
+  LPRTooltip: function (e) {
+    wx.showModal({
+      title: 'LPR（贷款市场报价利率）',
+      content: '自2019年10月8日起，新发放商业性个人住房贷款利率从以往的贷款基准利率（4.9%）转换为LPR（贷款市场报价利率），最终的商业贷款利率是通过在LPR的基础上增加基点确认。\r\n\r\nLPR分为一年期和五年期，于每月20日（遇节假日顺延）报价，可在中国人民银行网站查询。',
+      confirmText: '知道了',
+      showCancel: false
+    });
+  },
+  /**
+   * LPR
+   */
+  LPRValueInput: function (e) {
+    let value = e.detail.value || 0.0;
+    this.setData({
+      LPRValue: value,
+      LPRValueText: Math.floor(value * 10000) / 10000
+    })
+    this.calculateLPRRateValue();
+  },
+  /**
+   * 基点
+   */
+  basePointInput: function (e) {
+    let value = e.detail.value || 0;
+    this.setData({
+      basePoint: value,
+      basePointText: Math.floor(value * 100) / 100
+    })
+    this.calculateLPRRateValue();
+  },
+  /**
+   * 计算LPR 实际利率
+   */
+  calculateLPRRateValue: function (e) {
+    let value = (Math.floor(this.data.LPRValue * 10000) + Math.floor(this.data.basePoint * 100))/ 10000;
+    this.setData({
+      LPRRateValue: value
+    })
+  },
+  /**
+   * 商业贷款基准利率变更
+   */
   businessLoanRateChange: function (e) {
-    var index = e.detail.value;
+    let index = e.detail.value;
     this.setData({
       businessLoanRateIndex: index
     })
   },
+  /**
+   * 公积金利率选择
+   */
   gjjLoanRateChange: function (e) {
-    var index = e.detail.value;
+    let index = e.detail.value;
     this.setData({
       gjjLoanRateIndex: index
     })
   },
+  /**
+   * 计算
+   */
   calculateLoan: function () {
-    var mortgageData = this.getMortgageData();
+    let mortgageData = this.getMortgageData();
     switch (mortgageData.loanType) {
       case "1":
         if (!mortgageData.businessTotalLoan) {
